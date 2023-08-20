8516f4e5978a20404d56c2737f14e8bc6aac0b52
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-08-20 17:30:43 +0600

    Cart and modals javascript added

--- Script/index.js
+++ Script/index.js
@@ -1,1 +1,15 @@
-console.log("hello from javascript");
+// Modal Button Actions 
+
+document.getElementById('modal-btn').addEventListener('click', function(){
+    window.location.href = 'index.html';
+})
+
+const cartList = document.getElementById("cart-list");
+function addToCart(target){
+    const cardName = target.childNodes[3].childNodes[3].innerText;
+    const li = document.createElement('li');
+    li.innerText = cardName;
+    cartList.appendChild(li);
+    // target.childNodes[3].childNodes[5].childNodes[0].innerText
+    console.log(cardName);
+}

961c4b39c308eb0801f3b21687625e6b72144219
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-08-21 16:21:55 +0600

    Last push

--- Script/index.js
+++ Script/index.js
@@ -1,4 +1,5 @@
-// Univarsel Script
+// Univarsel Script Works by Default
+
 let applyBtn = document.getElementById('coupon-btn');
 applyBtn.disabled = true;
 let makePurchase = document.getElementById('makePurchase-btn');
@@ -11,6 +12,7 @@ document.getElementById('modal-btn').addEventListener('click', function(){
 // JS functions 
 
 function couponApply(){
+    // Getting Elements by id
     const discountField = document.getElementById('discount-field');
     const priceTotalText = document.getElementById('Total-price').innerText;
     const priceTotal = parseFloat(priceTotalText);
@@ -18,6 +20,8 @@ function couponApply(){
     let total = parseFloat(totalField.innerText);
     const inputField = document.getElementById('coupon-input');
     const inputValueText = inputField.value;
+
+    // Updating Elements value
     if(inputValueText === 'SELL200'){
         const discount = priceTotal*.20;
         total = priceTotal-discount;
@@ -37,20 +41,26 @@ function couponApply(){
 let totalPrice = 0;
 function addToCart(target){
     const cardName = target.childNodes[3].childNodes[3].innerText;
+
+    // Creating list item and adding
+
     const cartList = document.getElementById("cart-list");
     const li = document.createElement('li');
     li.innerText = cardName;
     cartList.appendChild(li);
+
+    // Total price update
     const priceText = target.childNodes[3].childNodes[5].childNodes[0].innerText;
     const price = parseFloat(priceText);
-
-    const totalField = document.getElementById('total');
-
     totalPrice += price;
     const priceTotal = document.getElementById('Total-price');
     priceTotal.innerText = totalPrice.toFixed(2);
     
+    // Cart Total
+    const totalField = document.getElementById('total');
     totalField.innerText = totalPrice.toFixed(2);
+
+    // Buttons condition
     if(totalPrice >= 200){
         
         applyBtn.disabled = false;
